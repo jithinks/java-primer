88725b07992095103ed2e330e9d805f77f2cc632
jithinks <ks.jithin@gmail.com>
2017-12-07 20:53:32 -0500

    Example of "divide and Conquer rule" added

---
+++ src/main/java/com/jks/startup/thread/divideandconquerrule/AddExecutors.java
@@ -0,0 +1,71 @@
+package com.jks.startup.thread.divideandconquerrule;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class AddExecutors {
+
+    private Integer parallelSum(){
+        long t1 = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<Future<Integer>> list = new ArrayList<Future<Integer>>();
+        int count=1;
+        int prev=0;
+        for(int i=1 ;i< 101;i++) {
+            if(count%2==0) {
+                System.out.println("Prev :" + prev + " current: " + i);
+                Future<Integer> future = executor.submit(new CallableAdder(prev,i));
+                list.add(future);
+                count=1;
+                continue;
+            }
+            prev=i ;
+            count++;
+        }
+        executor.shutdown();
+        int totsum=0;
+        for(Future<Integer> fut : list){
+            try {
+                totsum = totsum+ fut.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("parallel Total Sum is " + totsum);
+       long t2 = System.currentTimeMillis();
+        System.out.println("Time taken by parallelSum " + (t2-t1));
+        return totsum;
+    }
+
+
+    private int sequentialSum(){
+        long t1 = System.currentTimeMillis();
+        Integer totsum=0;
+        for(int i=1 ;i<101;i++){
+            totsum=totsum+i;
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println("sequentialSum Total Sum is " + totsum);
+        System.out.println("Time taken by sequentialSum " + (t2-t1));
+        return totsum;
+    }
+
+    /**
+     * https://dzone.com/articles/java-callable-future-understanding
+     *
+     * NOTE:The performance-wise Sequential sum is faster than the parallel sum
+     **/
+    public static void main(String[] args) {
+        AddExecutors adder = new AddExecutors();
+        int pSum= adder.parallelSum();
+        int sSum= adder.sequentialSum();
+        System.out.println("parallel Sum equals to Sequential Sum ? " );
+        System.out.println("Answer is :: " + (pSum==sSum));
+    }
+}
---
+++ src/main/java/com/jks/startup/thread/divideandconquerrule/CallableAdder.java
@@ -0,0 +1,18 @@
+package com.jks.startup.thread.divideandconquerrule;
+
+import java.util.concurrent.Callable;
+
+public class CallableAdder implements Callable{
+    private Integer operand1;
+    private Integer operand2;
+
+    CallableAdder(Integer operand1,Integer operand2){
+        this.operand1=operand1;
+        this.operand2=operand2;
+    }
+
+    public Integer call() throws Exception {
+        System.out.println(Thread.currentThread().getName()+" says : partial Sum for " + operand1 + " and "+ operand2+ " is "  +(operand1+operand2));
+        return operand1+operand2;
+    }
+}
